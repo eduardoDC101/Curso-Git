42a8f82e73fae3e11145a572901f4d68450d52c9
Eduardo Dourado <edutnb2018@gmail.com>
2025-02-24 15:08:20 -0300

    Removendo arquivos extras

--- script.js
+++
@@ -1,6 +0,0 @@
-alert("Testando o Javasript");
-const objeto = [
-  { nome: "Carlos", cpf: "123.123.123-11", idade: 18, sexo: "masculino" },
-  { nome: "Lucass", cpf: "323.323.323-31", idade: 14, sexo: "masculino" },
-  { nome: "Pedro", cpf: "143.143.143-41", idade: 35, sexo: "masculino" },
-];
